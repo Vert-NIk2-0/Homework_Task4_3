150f2da2e378afa93c43c546024676350e03d118
Nikert <2906026@mail.ru>
2024-08-07 17:35:10 +0300

    init

--- src/JavaTask4.java
+++ src/JavaTask4.java
@@ -13,16 +13,31 @@ public class JavaTask4 {
             if (stringMass[i].length() > 3) {
                 if (stringMass[i].length() > 6) {
                     charMass = stringMass[i].toCharArray();
-                    if ((charMass[3] >= 'A' && charMass[3] <= 'Z') || (charMass[3] >= 'a' && charMass[3] <= 'z') || (charMass[3] >= 'А' && charMass[3] <= 'Я') || (charMass[3] >= 'а' && charMass[3] <= 'я')) {
+                    if (
+                        (charMass[3] >= 'A' && charMass[3] <= 'Z') ||
+                        (charMass[3] >= 'a' && charMass[3] <= 'z') ||
+                        (charMass[3] >= 'А' && charMass[3] <= 'Я') ||
+                        (charMass[3] >= 'а' && charMass[3] <= 'я')
+                    ) {
                         charMass[3] = '#';
                     }
-                    if ((charMass[6] >= 'A' && charMass[6] <= 'Z') || (charMass[6] >= 'a' && charMass[6] <= 'z') || (charMass[6] >= 'А' && charMass[6] <= 'Я') || (charMass[6] >= 'а' && charMass[6] <= 'я')) {
+                    if (
+                        (charMass[6] >= 'A' && charMass[6] <= 'Z') ||
+                        (charMass[6] >= 'a' && charMass[6] <= 'z') ||
+                        (charMass[6] >= 'А' && charMass[6] <= 'Я') ||
+                        (charMass[6] >= 'а' && charMass[6] <= 'я')
+                    ) {
                         charMass[6] = '#';
                     }
                     stringMass[i] = String.valueOf(charMass);
                 }else {
                     charMass = stringMass[i].toCharArray();
-                    if ((charMass[3] >= 'A' && charMass[3] <= 'Z') || (charMass[3] >= 'a' && charMass[3] <= 'z') || (charMass[3] >= 'А' && charMass[3] <= 'Я') || (charMass[3] >= 'а' && charMass[3] <= 'я')) {
+                    if (
+                        (charMass[3] >= 'A' && charMass[3] <= 'Z') ||
+                        (charMass[3] >= 'a' && charMass[3] <= 'z') ||
+                        (charMass[3] >= 'А' && charMass[3] <= 'Я') ||
+                        (charMass[3] >= 'а' && charMass[3] <= 'я')
+                    ) {
                         charMass[3] = '#';
                     }
                     stringMass[i] = String.valueOf(charMass);
